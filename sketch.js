e43a55a559cf5d09cd81a83adb940006fd0df4ba
HOHO-P <honick9@gmail.com>
2025-05-12 10:27:12 +0800

    Add files via upload

---
+++ sketch.js
@@ -0,0 +1,109 @@
+let facemeshModel;
+let video;
+let predictions = [];
+let particles = []; // 粒子陣列
+
+function setup() {
+  createCanvas(640, 480);
+  video = createCapture(VIDEO);
+  video.size(width, height);
+  video.hide();
+
+  // 載入 facemesh 模型
+  loadFacemeshModel();
+}
+
+function draw() {
+  background(220);
+
+  // 顯示攝影機畫面
+  image(video, 0, 0, width, height);
+
+  // 如果有偵測到臉部特徵，繪製嘴唇並添加特效
+  if (predictions.length > 0) {
+    stroke('red');
+    strokeWeight(5);
+    noFill();
+
+    for (let prediction of predictions) {
+      const keypoints = prediction.scaledMesh;
+
+      // 嘴唇的索引
+      const lipsIndices = [
+        61, 146, 91, 181, 84, 17, 314, 405, 321, 375, 291, 61
+      ];
+
+      // 繪製嘴唇輪廓
+      beginShape();
+      for (let index of lipsIndices) {
+        const [x, y] = keypoints[index];
+        vertex(x, y);
+
+        // 在嘴唇周圍生成粒子
+        particles.push(new Particle(x, y));
+      }
+      endShape(CLOSE);
+    }
+  }
+
+  // 更新並顯示粒子效果
+  for (let i = particles.length - 1; i >= 0; i--) {
+    particles[i].update();
+    particles[i].show();
+
+    // 如果粒子已經消失，將其從陣列中移除
+    if (particles[i].finished()) {
+      particles.splice(i, 1);
+    }
+  }
+}
+
+async function loadFacemeshModel() {
+  facemeshModel = await facemesh.load();
+  console.log('Facemesh model loaded');
+
+  // 啟動偵測
+  detectFace();
+}
+
+async function detectFace() {
+  if (facemeshModel && video.loadedmetadata) {
+    const predictionsArray = await facemeshModel.estimateFaces({
+      input: video.elt,
+    });
+    predictions = predictionsArray;
+  }
+
+  // 持續偵測
+  requestAnimationFrame(detectFace);
+}
+
+// 粒子類別
+class Particle {
+  constructor(x, y) {
+    this.x = x;
+    this.y = y;
+    this.vx = random(-1, 1); // 粒子的水平速度
+    this.vy = random(-1, 1); // 粒子的垂直速度
+    this.alpha = 255; // 粒子的透明度
+  }
+
+  // 更新粒子位置和透明度
+  update() {
+    this.x += this.vx;
+    this.y += this.vy;
+    this.alpha -= 5; // 每次更新減少透明度
+  }
+
+  // 繪製粒子
+  show() {
+    noStroke();
+    fill(255, 0, 0, this.alpha); // 紅色粒子，透明度隨時間減少
+    ellipse(this.x, this.y, 8); // 粒子的大小
+  }
+
+  // 判斷粒子是否消失
+  finished() {
+    return this.alpha <= 0;
+  }
+}
